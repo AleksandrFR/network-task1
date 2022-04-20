56a83bdfbffb91c65c43758d885c008d5351fe57
AleksandrFR <79194849399@yandex.ru>
2022-04-20 20:34:06 +0300

    update commit network-tast2 realization

--- src/main/java/ClientSocketApp.java
+++ src/main/java/ClientSocketApp.java
@@ -3,19 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientSocketApp {
 
     public static void main(String[] args) {
-        String host = "localhost";
+        Scanner scanner = new Scanner(System.in);
+        String host = "netology.homework";
         int port = 8089;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("GET / HTTP/1.1\n +" +
-                    "Host: localhost\n\n\n");
-            String resp = in.readLine();
-            System.out.println(resp);
+            System.out.println(in.readLine());
+            out.println(scanner.nextLine());
+            System.out.println(in.readLine());
+            out.println(scanner.nextLine());
+            System.out.println(in.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/SocketApp.java
+++ src/main/java/SocketApp.java
@@ -8,14 +8,20 @@ import java.net.Socket;
 public class SocketApp {
     public static void main(String[] args) throws IOException {
         int port = 8089;
+
         while (true) {
             ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
             Socket clientSocket = serverSocket.accept(); // ждем подключения
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             System.out.printf("New connection accepted. Port.%d%n", clientSocket.getPort());
-            final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+            out.println("Hi visitor, what is your name?");
+            String username = in.readLine();
+            out.println(username + ", are you child? (yes/no)");
+            String ansver = in.readLine();
+            out.println(ansver.equals("yes") ? "Welcome to the kids area, " + username + "! Let's play!" :
+                    "Welcome to the adult zone, " + username + "! Have a good rest, or a good working day!");
             serverSocket.close();
         }
     }
